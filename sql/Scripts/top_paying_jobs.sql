3e0c92a187a986355724810b5631770c5fef72fa
Aytaj Nurullazade <nrllzda@gmail.com>
2026-01-31 04:30:01 +0200

    Analyze top paying jobs

---
+++ sql/Scripts/top_paying_jobs.sql
@@ -0,0 +1,24 @@
+/* 
+Question: What are the top-paying data analyst jobs?
+- Identify the top 10 highest-paying Data Analyst roles that are available remotely.
+ */
+
+select
+	job_id,
+	job_title,
+	company_dim.name as company_name,
+	job_location,
+	job_schedule_type,
+	salary_year_avg,
+	job_posted_date
+from 
+	job_postings_fact 
+left join company_dim on company_dim.company_id = job_postings_fact.company_id 
+where 
+	job_location = 'Anywhere' and 
+	job_title_short = 'Data Analyst' and 
+	salary_year_avg is not null
+order by salary_year_avg desc
+limit 10
+
+
